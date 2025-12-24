07bd54b79f74d091546259e5f8622aa2ab2ea3a5
Vinícius <90075209+ViniciusCassemira@users.noreply.github.com>
2025-12-23 22:55:09 -0300

    Simplify setup script by removing Zsh check
    Removed shell check and change for Zsh.

--- setup.sh
+++ setup.sh
@@ -2,11 +2,6 @@ chmod -R +x ./
 
 sudo apt update && sudo apt install -y zsh
 
-if [ "$SHELL" != "$(command -v zsh)" ]; then
-  echo "⚠️ Zsh is not the default shell. Changing now..."
-  chsh -s $(command -v zsh)
-fi
-
 echo "🗣️ After Oh My Zsh installation:"
 echo ">_ ./setup2.sh"
 ./scripts/ohmyzsh/install.sh
