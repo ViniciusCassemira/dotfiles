31c1ee3efbc0d16cbe416b9fc1be35b6c813b15f
ViniciusCassemira <vinicius.cassemira@fatec.sp.gov.br>
2025-12-26 09:44:48 -0300

    fix: zsh default shell config

--- setup.sh
+++ setup.sh
@@ -2,6 +2,14 @@ chmod -R +x ./
 
 sudo apt update && sudo apt install -y zsh
 
+if [ "$SHELL" != "$(command -v zsh)" ]; then
+  echo "⚠️ config zsh as default shell"
+  chsh -s "$(command -v zsh)"
+  echo "✓ Zsh defined as default shell"
+
+  export SHELL="$(command -v zsh)"
+fi
+
 echo "🗣️ After Oh My Zsh installation:"
 echo ">_ ./setup2.sh"
-./scripts/ohmyzsh/install.sh
+zsh ./scripts/ohmyzsh/install.sh
